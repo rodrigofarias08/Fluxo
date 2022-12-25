1aa615dfdb9a163a3f69d3fb758a910cf6aa95e7
Rodrigo Farias <rodrigofarias08@gmail.com>
2022-12-24 21:57:18 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Olá Mundo!!</h1>
+      <ul className="lista">
+        <li>Conceitos de React;</li>
+        <li>Componentes.</li>
+      </ul>
+    </div>
+  );
+}
